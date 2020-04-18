f6f8948afa9b92a295b43accd8efedd5a965def0
A2t1p1 <48638366+A2t1p1@users.noreply.github.com>
2020-04-18 15:50:32 -0600

    Proyecto

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Torres_Anibal_Parcial
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Proyecto());
         }
     }
 }
